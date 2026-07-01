5e3599aef8ec9a1cbc26635ef7354b3270304a2a
Marcheloo-7 <marcelochiriboga246@gmail.com>
2026-07-01 09:49:50 -0500

    Limpiar repositorio

--- backend/app/services/student.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import Session
-from app.models.student import Student
-
-class StudentService:
-
-    def get_students(self, db: Session):
-        return db.query(Student).all()
